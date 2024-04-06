eb3fe5fbae338d22f4b07f11f7d81a93d19bd1a1
Ryan Yarbery <Yarbery@Yahoo.com>
2024-04-06 15:31:41 -0700

    Managing parameters

--- dydxTransact test 7.py
+++ dydxTransact test 7.py
@@ -6,20 +6,21 @@ from dydx3.constants import MARKET_ETH_USD
 from dydx3.constants import NETWORK_ID_SEPOLIA
 from dydx3.constants import ORDER_SIDE_BUY, ORDER_SIDE_SELL
 from dydx3.constants import ORDER_TYPE_LIMIT
-from dydx3.constants import ORDER_STATUS_OPEN
 
-"""
+def place_limit_order(side, size, price, post_only=True, limit_fee='0.0015', expiration_seconds=300, position_id=None):
+    """
     Place a limit order on the dYdX exchange.
 
-    :param side_input: 'buy' or 'sell' (case insensitive) indicating the order side.
-    :param size: The amount of the asset to trade (as a string or float), e.g., '0.5' for 0.5 ETH.
-    :param price: The limit price at which to execute the order (as a string or float), e.g., '3348' for $3348.
-    :param expiration_minutes: The time in minutes until the order expires (as an integer), default is 5 minutes.
+    :param side: 'BUY' or 'SELL' indicating the order side.
+    :param size: The amount of the asset to trade, e.g., 0.5 for 0.5 ETH.
+    :param price: The limit price at which to execute the order, e.g., 3348 for $3348.
+    :param post_only: Whether the order should be post only, default is True.
+    :param limit_fee: The fee rate for the order, default is '0.0015'.
+    :param expiration_minutes: The time in minutes until the order expires, default is 5 minutes.
+    :param position_id: Optional custom position ID. If not provided, it's fetched from the account info.
 
     :return: The response from the order request as a dictionary.
     """
-
-def place_limit_order(side_input, size, price, expiration_minutes=5):
     api_key = os.getenv('dydxKey')
     api_secret = os.getenv('dydxSecret')
     passphrase = os.getenv('dydxPassphrase')
@@ -36,34 +37,27 @@ def place_limit_order(side_input, size, price, expiration_minutes=5):
         },
     )
 
+    # Derive and set the STARK private key
     stark_key_data = client.onboarding.derive_stark_key()
     client.stark_private_key = stark_key_data['private_key']
 
-    account_response = client.private.get_account().data
-    position_id = account_response['account']['positionId']
+    # Fetch the position ID if not provided
+    if not position_id:
+        account_response = client.private.get_account().data
+        position_id = account_response['account']['positionId']
 
-    market = MARKET_ETH_USD
-
-    if side_input.lower() == 'buy':
-        side = ORDER_SIDE_BUY
-    elif side_input.lower() == 'sell':
-        side = ORDER_SIDE_SELL
-    else:
-        raise ValueError("Invalid order side. Must be 'buy' or 'sell'.")
-
-    order_type = ORDER_TYPE_LIMIT
-
-    expiration_time = int(time.time()) + expiration_minutes * 60
+    # Set the expiration time
+    expiration_time = int(time.time()) + expiration_seconds
 
     order_params = {
         'position_id': position_id,
-        'market': market,
+        'market': MARKET_ETH_USD,
         'side': side,
-        'order_type': order_type,
-        'size': size,
-        'post_only': False,
-        'price': price,
-        'limit_fee': '0.0015',
+        'order_type': ORDER_TYPE_LIMIT,
+        'size': str(size),
+        'post_only': post_only,
+        'price': str(price),
+        'limit_fee': limit_fee,
         'expiration_epoch_seconds': expiration_time,
     }
 
@@ -71,10 +65,10 @@ def place_limit_order(side_input, size, price, expiration_minutes=5):
 
     return order_response.data
 
-# Example usage:
+# Example usage
 if __name__ == "__main__":
-    side = 'buy'  # or 'sell'
-    size = '0.5'  # Amount of ETH
-    price = '3348'  # Price in USD
+    side = ORDER_SIDE_BUY  # or ORDER_SIDE_SELL
+    size = 0.5  # Amount of ETH
+    price = 3348  # Price in USD
     result = place_limit_order(side, size, price)
     print("Order response:", result)
