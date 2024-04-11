a613aaeb29ebb17a708211fb00616799258d773a
Ryan Yarbery <Yarbery@Yahoo.com>
2024-04-10 17:48:46 -0700

    Removed unecessary text

--- dydxInterface.py
+++ dydxInterface.py
@@ -116,7 +116,6 @@ def fetch_eth_market_data():
 
     return eth_details
 
-# Need to rename to order_id
 def cancel_order(order_id):
     """
     Cancels an order on the dYdX exchange given a position_id.
