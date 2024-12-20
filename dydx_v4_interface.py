64c644224b5d505cfb3a0940edd11d1bd46ea3cf
Ryan Yarbery <Yarbery@Yahoo.com>
2024-12-20 10:26:12 -0800

    Minor changes

--- dydx_v4_interface.py
+++ dydx_v4_interface.py
@@ -581,9 +581,9 @@ async def main():
     # print('Market info: ', market_info)
     # response = await dydx_interface.cancel_all_orders()
     # print(f'Orders Cancelled = {response}')
-    # response = await dydx_interface.clear_existing_orders_and_positions()
-    free_collateral = await dydx_interface.fetch_free_collateral()
-    print("Free collateral: ", free_collateral)
+    response = await dydx_interface.clear_existing_orders_and_positions()
+    # free_collateral = await dydx_interface.fetch_free_collateral()
+    # print("Free collateral: ", free_collateral)
    
 
 if __name__ == "__main__":
